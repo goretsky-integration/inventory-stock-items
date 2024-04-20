0c0064c2aeb4ffeb4db91458168fdd5b50df949b
Eldos <eldos.baktybekov@gmail.com>
2024-04-20 11:41:00 +0600

    :wrench: Add sentry in config

--- src/config.py
+++ src/config.py
@@ -9,12 +9,21 @@ __all__ = ('load_config_from_file', 'Config')
 
 
 @dataclass(frozen=True, slots=True)
+class SentryConfig:
+    is_enabled: bool
+    dsn: str
+    traces_sample_rate: float
+    profiles_sample_rate: float
+
+
+@dataclass(frozen=True, slots=True)
 class Config:
     timezone: ZoneInfo
     units_storage_base_url: str
     auth_credentials_storage_base_url: str
     country_code: CountryCode
     message_queue_url: str
+    sentry: SentryConfig
 
 
 def load_config_from_file(file_path: pathlib.Path) -> Config:
@@ -29,10 +38,19 @@ def load_config_from_file(file_path: pathlib.Path) -> Config:
     country_code = CountryCode[config['country_code'].upper()]
     message_queue_url = config['message_queue']['url']
 
+    sentry_config = config['sentry']
+    sentry = SentryConfig(
+        is_enabled=sentry_config['is_enabled'],
+        dsn=sentry_config['dsn'],
+        traces_sample_rate=sentry_config['traces_sample_rate'],
+        profiles_sample_rate=sentry_config['profiles_sample_rate'],
+    )
+
     return Config(
         timezone=timezone,
         units_storage_base_url=units_storage_base_url,
         auth_credentials_storage_base_url=auth_credentials_storage_base_url,
         country_code=country_code,
         message_queue_url=message_queue_url,
+        sentry=sentry,
     )
