39a7657dbf4288afe083e83616a8b5fd1aae023c
Eldos <eldos.baktybekov@gmail.com>
2024-06-03 22:18:39 +0600

    :sparkles: Add `get inventory stocks` API call

--- src/dodo_is.py
+++ src/dodo_is.py
@@ -1,8 +1,16 @@
+from collections.abc import Iterable
+from uuid import UUID
+
 import httpx
+from pydantic import SecretStr
 
 from new_types import DodoISConnectionHttpClient
 
 __all__ = ('DodoISConnection',)
+
+
+def merge_uuids(uuids: Iterable[UUID]) -> str:
+    return ','.join(str(uuid) for uuid in uuids)
 
 
 class DodoISConnection:
@@ -10,16 +18,25 @@ class DodoISConnection:
     def __init__(self, http_client: DodoISConnectionHttpClient):
         self.__http_client = http_client
 
-    def get_inventory_stocks(
+    async def get_inventory_stocks(
             self,
             *,
-            unit_id: int,
-            cookies: dict[str, str],
+            unit_uuids: Iterable[UUID],
+            access_token: SecretStr,
+            take: int = 1000,
+            skip: int = 0,
     ) -> httpx.Response:
-        url = '/OfficeManager/StockBalance/Get'
-        request_query_params = {'unitId': unit_id}
-        return self.__http_client.get(
+        request_query_params = {
+            'units': merge_uuids(unit_uuids),
+            'take': take,
+            'skip': skip,
+        }
+        url = '/accounting/inventory-stocks'
+        headers = {
+            'Authorization': f'Bearer {access_token.get_secret_value()}',
+        }
+        return await self.__http_client.get(
             url=url,
             params=request_query_params,
-            cookies=cookies,
+            headers=headers,
         )
