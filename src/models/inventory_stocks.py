b8709889d7c53f44e684e0524798ab70c8e36167
Eldos <eldos.baktybekov@gmail.com>
2024-06-03 19:29:59 +0600

    :label: Update `InventoryStockItem` model due to Dodo IS API migrate

--- src/models/inventory_stocks.py
+++ src/models/inventory_stocks.py
@@ -1,11 +1,24 @@
-from pydantic import BaseModel
+from datetime import datetime
+from typing import Annotated
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+from enums import MeasurementUnit
 
 __all__ = ('InventoryStockItem',)
 
 
 class InventoryStockItem(BaseModel):
     name: str
-    unit_id: int
+    unit_id: Annotated[UUID, Field(validation_alias='unitId')]
     quantity: float
-    measurement_unit: str
-    days_until_balance_runs_out: int
+    measurement_unit: Annotated[
+        MeasurementUnit,
+        Field(validation_alias='measurementUnit'),
+    ]
+    days_until_balance_runs_out: Annotated[
+        int,
+        Field(validation_alias='daysUntilBalanceRunsOut'),
+    ]
+    calculated_at: Annotated[datetime, Field(validation_alias='calculatedAt')]
