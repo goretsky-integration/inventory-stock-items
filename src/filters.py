2c52d77492f7627f6b645e40f70f734592b7eaa4
Eldos <eldos.baktybekov@gmail.com>
2024-06-07 15:09:29 +0600

    :sparkles: Add filtering

--- src/filters.py
+++ src/filters.py
@@ -10,13 +10,30 @@ __all__ = (
 )
 
 
+def is_all_zero(*numbers: int | float) -> bool:
+    return all(number == 0 for number in numbers)
+
+
 def filter_relevant_items(
         items: Iterable[InventoryStockItem],
 ) -> list[InventoryStockItem]:
-    return [
-        item for item in items
-        if '(не использовать)' not in item.name.lower()
-    ]
+    relevant_items: list[InventoryStockItem] = []
+
+    for item in items:
+        if '(не использовать)' in item.name.lower():
+            continue
+        if is_all_zero(
+            item.average_weekend_expense,
+            item.average_weekday_expense,
+            item.balance_in_money,
+            item.quantity,
+            item.days_until_balance_runs_out,
+        ):
+            continue
+
+        relevant_items.append(item)
+
+    return relevant_items
 
 
 def filter_running_out_stock_items(
